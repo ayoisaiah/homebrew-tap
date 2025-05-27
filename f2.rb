c42c8eb5714e21246dc3965423719a6dce6cdee4
goreleaserbot <goreleaser@carlosbecker.com>
2025-05-27 09:06:58 +0100

    Brew formula update for f2 version v2.1.2

--- f2.rb
+++ f2.rb
@@ -5,22 +5,22 @@
 class F2 < Formula
   desc "F2 is a cross-platform command-line tool for batch renaming files and directories quickly and safely"
   homepage "https://f2.freshman.tech"
-  version "2.1.1"
+  version "2.1.2"
 
   depends_on "exiftool" => :optional
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/ayoisaiah/f2/releases/download/v2.1.1/f2_2.1.1_darwin_amd64.tar.gz"
-      sha256 "fe58c97fc1ce1ae8d174cce0964187f3c708404388ca12fcc89e34329369cc36"
+      url "https://github.com/ayoisaiah/f2/releases/download/v2.1.2/f2_2.1.2_darwin_amd64.tar.gz"
+      sha256 "4463d3d5d137d5ddb94f7cbfece7d35b4c0950c56e151f980d07306a49a5ad30"
 
       def install
         bin.install "f2"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/ayoisaiah/f2/releases/download/v2.1.1/f2_2.1.1_darwin_arm64.tar.gz"
-      sha256 "e48609a0f6b77cf0c2ebbd337e32a1ede6ff30ac7328410653dab312f358c1f4"
+      url "https://github.com/ayoisaiah/f2/releases/download/v2.1.2/f2_2.1.2_darwin_arm64.tar.gz"
+      sha256 "4f70b60248ab4293a4f34e79a17e353284bd93e386089a0242914830b7e1b2b3"
 
       def install
         bin.install "f2"
@@ -30,15 +30,15 @@ class F2 < Formula
 
   on_linux do
     if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
-      url "https://github.com/ayoisaiah/f2/releases/download/v2.1.1/f2_2.1.1_linux_amd64.tar.gz"
-      sha256 "03646689c0fd1e6c2cf6c8138119831d259fd854eb066c2d0327286b2000e5b9"
+      url "https://github.com/ayoisaiah/f2/releases/download/v2.1.2/f2_2.1.2_linux_amd64.tar.gz"
+      sha256 "06b3ee1209ced9bcf8c471925118ca01a119d191007c9e698fc18082155ea148"
       def install
         bin.install "f2"
       end
     end
     if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
-      url "https://github.com/ayoisaiah/f2/releases/download/v2.1.1/f2_2.1.1_linux_arm64.tar.gz"
-      sha256 "5550bfb720038552cd99ab37c14f4740e5e64c691d68f7ec325e844eb0c8dea6"
+      url "https://github.com/ayoisaiah/f2/releases/download/v2.1.2/f2_2.1.2_linux_arm64.tar.gz"
+      sha256 "825b5485ee6904415ec712b3e67cc77ae9e8be3421d3f469b9509a34121669c9"
       def install
         bin.install "f2"
       end
